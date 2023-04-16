08054709d01f60520a70d08f5298400b680d855d
kaascevich <swimmer-flowing-0s@icloud.com>
2023-04-15 22:47:45 -0400

    Began work on PaneControl

--- PaneKit/Sources/Controls/PaneControl.swift
+++ PaneKit/Sources/Controls/PaneControl.swift
@@ -5,4 +5,16 @@
 //  Created by Kaleb on 4/15/23.
 //
 
-import Foundation
+import SwiftUI
+
+public protocol PaneControl: Codable {
+    associatedtype Value: Codable
+    associatedtype Content: View
+    
+    static var type: String { get }
+    
+    var value: Value { get set }
+    var name: String { get }
+    
+    var content: Content { get }
+}
